8e1e99a2c17fa385402fdc27a4e1fb17364fb154
piche-r <144396834+piche-r@users.noreply.github.com>
2023-12-09 14:09:01 -0500

    Update vn100_analysis_tunnel1.m
    Changed magnetometer correction info, deleted driving in circles bag, deleted high pass filter from gyro data.

--- vn100_analysis_tunnel1.m
+++ vn100_analysis_tunnel1.m
@@ -3,17 +3,14 @@ clear all; close all;
 %% Read in bag file
 
 tunnel1_bag = rosbag("tunnel1_vn100.bag");
-circles_bag = rosbag("driving_in_circles.bag");
 
 % Select topics
 tunnel1_bag_imu = select(tunnel1_bag, 'Topic', '/imu');
 tunnel1_bag_gps = select(tunnel1_bag, 'Topic', '/gps');
-circles_bag_imu = select(circles_bag, 'Topic', '/imu');
 
 % Read messages, return cell array of structures
 tunnel1_imu_msgs = readMessages(tunnel1_bag_imu, 'DataFormat','struct');
 tunnel1_gps_msgs = readMessages(tunnel1_bag_gps, 'DataFormat','struct');
-circles_imu_msgs = readMessages(circles_bag_imu, 'DataFormat','struct');
 
 % Get Time
 tunnel1_times = tunnel1_bag_imu.MessageList.Time;
@@ -36,14 +33,7 @@ for ind = 1:size(tunnel1_imu_msgs)
       tunnel1_mag(ind,3) = tunnel1_imu_msgs{ind}.MagField.MagneticField_.Z;
 end
 
-for ind = 1:size(circles_imu_msgs)
-      circles_mag(ind,1) = circles_imu_msgs{ind}.MagField.MagneticField_.X;
-      circles_mag(ind,2) = circles_imu_msgs{ind}.MagField.MagneticField_.Y;
-      circles_mag(ind,3) = circles_imu_msgs{ind}.MagField.MagneticField_.Z;
-end
-
 % Convert Magnetometer Units to Gauss
-circles_mag = circles_mag/10000;
 tunnel1_mag = tunnel1_mag/10000;
 
 % Extract GPS Data From Structs
@@ -65,9 +55,35 @@ tunnel1_gps_time = tunnel1_gps_time - tunnel1_gps_time(1);
 
 %% Magnetometer Calibration
 
-% Computer Hard and Soft Iron Magnetometer Coefficients
-x = circles_mag(:,1); % Extract x and y data for readability
-y = circles_mag(:,2);
+% Conduct PCA to fit Data inside an ellipse
+x = tunnel1_mag(:,1:2);
+
+% Sample-estimates of mean vector and covariance matrix
+mu = mean(x,1);
+Sigma = 4*cov(x);
+
+% Subtract the estimated mean vector to make the data 0-mean
+xzm = x - mu;
+
+% Get the eigenvectors (Q) and eigenvalues (D) of the estimated covariance matrix
+[Q,D] = eig(Sigma);
+
+% Sort the eigenvalues from large to small, reorder eigenvectors accordingly as well.
+[d,ind] = sort(diag(D),'descend');
+Q = Q(:,ind);
+D = diag(d);
+
+% Scale eigenvectors
+VV = Q*sqrt(D);
+
+% Create ellipse 
+t = linspace(0,2*pi,100);
+e = [cos(t) ; sin(t)]; 
+y = VV*e + mu';
+
+% x and y vectors for readability
+x = y(1,:)';
+y = y(2,:)';
 
 % Find best fit ellipse using non-linear least squares optimization
 init_weights = ones(5,1);
@@ -79,27 +95,29 @@ opt_weights = lsqnonlin(Objective_Function, init_weights, [], [], options);
 a = opt_weights(1); b = opt_weights(2); c = opt_weights(3); d = opt_weights(4); e = opt_weights(5); 
 num = 2 * ((a*e^2 - b*d*e + c*d^2)/(4*a*c - b^2)+1);
 % Major and Minor Axis
-maj_ax = sqrt(num/(a + c + sqrt((a-c)^2+b^2)));
-min_ax = sqrt(num/(a + c - sqrt((a-c)^2 + b^2))); 
+min_ax = sqrt(num/(a + c + sqrt((a-c)^2+b^2)));
+maj_ax = sqrt(num/(a + c - sqrt((a-c)^2 + b^2))); 
 % Center Coordinates
 center_x = (b*e - 2*c*d)/(4*a*c - b^2); 
 center_y = (b*d - 2*a*e)/(4*a*c - b^2);
 % Rotation Angle
 theta = 0.5 * atan(b/(a-c));
 
-% Correct Raw Magnetometer data
+% Compute Hard and Soft Iron Magnetometer Coefficients
 sigma = min_ax/maj_ax;
 S1 = sigma*cos(-theta)*cos(theta)-sin(-theta)*sin(theta);
 S2 = sigma*cos(-theta)*sin(theta)+sin(-theta)*cos(theta);
 S3 = -sigma*sin(-theta)*cos(theta)-cos(-theta)*sin(theta);
 S4 = -sigma*sin(-theta)*sin(theta)+cos(-theta)*cos(theta);
 S = [S1, S2; S3, S4];
-corr_data = S*([x y]' - [center_x; center_y]);
+
+% Correct Raw Magnetometer data
+corr_data = S*([tunnel1_mag(:,1), tunnel1_mag(:,2)]' - [center_x; center_y]);
 
 % Plot magnetometer data before and after calibration
 figure(1)
 hold on
-scatter(circles_mag(:,1),circles_mag(:,2),'r*')
+scatter(tunnel1_mag(:,1),tunnel1_mag(:,2),'r*')
 scatter(corr_data(1,:),corr_data(2,:),'g*')
 
 % Plot Ellipse approximations
@@ -111,38 +129,32 @@ plot(X,Y,'r--')
 plot(corr_ellipse(1,:),corr_ellipse(2,:),'g--')
 xlabel('X-Axis'), ylabel('Y-Axis')
 legend('Uncorrect Data', 'Corrected Data', 'Uncorrected Ellipse','Corrected Ellipse')
-%title('Corrected and Uncorrected Magnetometer Data'), fontname('Times New Roman'), grid on, axis equal, axis padded
+title('Corrected and Uncorrected Magnetometer Data'), fontname('Times New Roman'), grid on, axis equal, axis padded
 
 %% Sensor Fusion
 
 %  Raw Magnetometer Yaw Angle
-tunnel1_mag_yaw = (180/pi) * (atan2(tunnel1_mag(:,1),tunnel1_mag(:,2)));
+tunnel1_mag_yaw = (180/pi) * (atan2(-tunnel1_mag(:,1),tunnel1_mag(:,2)));
 
 % Corrected Magnetometer Yaw Angle
 corr_mag = S*(tunnel1_mag(:,1:2)' - [center_x; center_y]);
-corr_mag_yaw = (180/pi) * (atan2(corr_mag(1,:),corr_mag(2,:)));
+corr_mag_yaw = (180/pi) * (atan2(-corr_mag(1,:),corr_mag(2,:)));
 corr_mag_yaw = wrapTo180(corr_mag_yaw);
 
-% Plot Raw and Corrected Magnetometer Yaw
-figure(2) 
-hold on 
-scatter(tunnel1_mag(:,1),tunnel1_mag(:,2),'r*')
-scatter(corr_mag(1,:),corr_mag(2,:),'g*')
-plot(corr_ellipse(1,:),corr_ellipse(2,:),'g--')
-plot(X,Y,'r--')
-xlabel('X-Axis'), ylabel('Y-Axis')
-legend('Uncorrect Data', 'Corrected Data', 'Uncorrected Ellipse','Corrected Ellipse')
-%title('Corrected and Uncorrected Magnetometer Data'), fontname('Times New Roman'), grid on, axis equal, axis padded
+% Apply Low Pass Filter to Magnetometer Data
+fs = 1/mean(diff(tunnel1_times));
+filtered_mag_yaw = lowpass(corr_mag_yaw,0.001,fs);
 
-figure(3)
+figure(2)
 hold on
 plot(tunnel1_times,tunnel1_mag_yaw,'r-','LineWidth',1)
-plot(tunnel1_times,corr_mag_yaw,'g-','LineWidth',1)
+plot(tunnel1_times,corr_mag_yaw,'y-','LineWidth',1)
+plot(tunnel1_times,filtered_mag_yaw, '-g','LineWidth',2)
 xlabel('Time Series (s)')
 ylabel(['Yaw Angle (' char(176) ')'])
 title('Raw and Calibrated Magnetometer Yaw Angle')
-legend('Raw Magnetometer Yaw','Calibrated Magnetometer Yaw')
-%fontname('Times New Roman')
+legend('Raw Magnetometer Yaw','Calibrated Magnetometer Yaw','Filtered Magnetometer Yaw')
+fontname('Times New Roman')
 grid on
 
 % Integrate Angular Rate to get Yaw Angle
@@ -151,7 +163,7 @@ gyro_yaw = cumtrapz(tunnel1_times,tunnel1_angvel(:,3));
 gyro_yaw = (180/pi) * wrapToPi(gyro_yaw);
 
 % Plot Magnetometer Yaw and Gyro Yaw
-figure(4)
+figure(3)
 subplot(2,1,1)
 hold on
 plot(tunnel1_times,corr_mag_yaw,'m-','LineWidth',1)
@@ -165,44 +177,10 @@ xlabel('Time Series (s)')
 ylabel(['Yaw Angle (' char(176) ')'])
 title('Yaw from Integrated Angular Rate')
 grid on
-%fontname('Times New Roman')
-
-% Apply Low Pass Filter to Magnetometer Data
-fs = 1/mean(diff(tunnel1_times));
-filtered_mag_yaw = lowpass(corr_mag_yaw,0.1,fs);
-
-% Apply High Pass Filter to Gyro Data
-% filtered_gyro_rate = highpass(driving_angvel(:,3),1e-8,fs);
-[b,a] = butter(1, 0.004/(0.5/fs), "high");
-filtered_gyro_rate = filtfilt(b,a,tunnel1_angvel(:,3));
-filtered_gyro_yaw = cumtrapz(tunnel1_times,filtered_gyro_rate);
-filtered_gyro_yaw = (180/pi) * wrapToPi(filtered_gyro_yaw);
-
-% Plot Filtered Yaw Angles
-figure(5)
-subplot(2,1,1)
-hold on
-plot(tunnel1_times,corr_mag_yaw, '-r','LineWidth',1)
-plot(tunnel1_times,filtered_mag_yaw, '-g','LineWidth',1)
-xlabel('Time Series (s)')
-ylabel(['Yaw Angle (' char(176) ')'])
-legend('Unfiltered','Filtered')
-title('Yaw from Calibrated Magnetometer Data')
-grid on
-%fontname('Times New Roman')
-subplot(2,1,2)
-hold on
-plot(tunnel1_times,gyro_yaw, '-r','LineWidth',1')
-plot(tunnel1_times,filtered_gyro_yaw, '-g','LineWidth',1')
-xlabel('Time Series (s)')
-ylabel(['Yaw Angle (' char(176) ')'])
-legend('Unfiltered','Filtered')
-title('Yaw from Integrated Angular Rate Data')
-grid on
-%fontname('Times New Roman')
+fontname('Times New Roman')
 
 % Apply Complimentary Filter
-alpha = 0.9;
+alpha = 0.35;
 filtered_yaw = alpha*filtered_mag_yaw' + (1-alpha)*gyro_yaw;
 
 % True Yaw Angle 
@@ -214,10 +192,10 @@ for i = 1:length(x)
     yaw(i) = (180/pi) * atan2(2*(w(i)*z(i)+x(i)*y(i)),1-2*(y(i)*y(i)+z(i)*z(i)));
 end
 
-figure(6)
+figure(4)
 hold on
 plot(tunnel1_times,filtered_yaw,'-g','LineWidth',1')
-plot(tunnel1_times,filtered_gyro_yaw, '-b','LineWidth',1')
+plot(tunnel1_times,gyro_yaw, '-b','LineWidth',1')
 plot(tunnel1_times,filtered_mag_yaw,'-m','LineWidth',1')
 plot(tunnel1_times,yaw,'k-','LineWidth',1')
 xlabel('Time Series (s)')
@@ -225,7 +203,7 @@ ylabel(['Yaw Angle (' char(176) ')'])
 legend('Complimentary Filter','High Pass Filter - Gyro. Data','Low Pass Filter - Mag. Data', 'VN-100 Yaw')
 title('Yaw Angle with Low Pass, High Pass and Complimentary Filter Compared to IMU Yaw')
 grid on
-%fontname('Times New Roman')
+fontname('Times New Roman')
 
 %% Forward Velocity Estimate
 
@@ -236,7 +214,7 @@ vel_y = cumtrapz(tunnel1_times,tunnel1_accel(:,2) - mean(tunnel1_accel(:,2)));
 
 % Correct Velocity Data
 forward_vel = detrend(vel_x);
-forward_vel = 0.2*abs(forward_vel);
+forward_vel = abs(forward_vel);
 
 % Estimate Velocity From GPS Data
 d_lat = diff(tunnel1_utmnorth);
@@ -246,7 +224,7 @@ dt = diff(tunnel1_gps_time)';
 gps_vel = dx./dt;
 
 % Plot Corrected and Uncorrected Velocity from Accel. and Velocity from GPS
-figure(7)
+figure(5)
 hold on
 plot(tunnel1_times, raw_vel_x, '-r','LineWidth',1)
 plot(tunnel1_times, vel_x, '-y','LineWidth',1)
@@ -255,18 +233,18 @@ xlabel('Time Series (s)')
 ylabel('Velocity (m/s)')
 legend('Uncorrected','Acceleration Correction Applied','Velocity Correction Applied','Location','northwest')
 title('Corrected and Uncorrected Velocity Estimate from Integrated Forward Acceleration')
-%fontname('Times New Roman')
+fontname('Times New Roman')
 grid on
 axis padded
 
-figure(8)
+figure(6)
 subplot(2,1,1)
 hold on
 plot(tunnel1_times, forward_vel,'-m','LineWidth',1)
 xlabel('Time Series (s)')
 ylabel('Velocity (m/s)')
 title('Velocity Estimate from Integrated Forward Acceleration')
-%fontname('Times New Roman')
+fontname('Times New Roman')
 grid on
 subplot(2,1,2)
 hold on
@@ -274,7 +252,7 @@ plot(tunnel1_gps_time(1:20),gps_vel,'-b','LineWidth',1)
 xlabel('Time Series (s)')
 ylabel('Velocity (m/s)')
 title('Velocity Estimate from GPS')
-%fontname('Times New Roman')
+fontname('Times New Roman')
 grid on
 
 %% Dead Reckoning 
@@ -290,21 +268,21 @@ tunnel1_utmeast = tunnel1_utmeast - tunnel1_utmeast(1);
 gps_displacement = cumtrapz(tunnel1_gps_time(1:length(gps_vel)), gps_vel);
 
 % Plot GPS and IMU Displacement over time
-figure(9)
+figure(7)
 hold on 
 subplot(2,1,1)
 plot(tunnel1_times,imu_displacement,'m-','LineWidth',1)
 xlabel('Time Series (s)')
 ylabel('Displacement (m)')
 title('Displacement Estimate from IMU')
-%fontname('Times New Roman')
+fontname('Times New Roman')
 grid on
 subplot(2,1,2)
 plot(tunnel1_gps_time(1:20), gps_displacement,'b-','LineWidth',1)
 xlabel('Time Series (s)')
 ylabel('Displacement (m)')
 title('Displacement Estimate from GPS')
-%fontname('Times New Roman')
+fontname('Times New Roman')
 grid on
 
 % Calculate omega*velocity
@@ -313,7 +291,7 @@ omega_xdot = tunnel1_angvel(:,3).*vel_x;
 corr_omega_xdot = tunnel1_angvel(:,3).*forward_vel;
 y_obs = lowpass(tunnel1_accel(:,2),0.01,fs);
 
-figure(10)
+figure(8)
 subplot(3,1,1)
 hold on
 plot(tunnel1_times,omega_xdot, '-b', 'LineWidth',1)
@@ -322,7 +300,7 @@ legend('\omega x','y_{obs}')
 xlabel('Time Series (s)')
 ylabel('Acceleration (m/s^{2})')
 title('Y-Axis Acceleration - Uncorrected and Unfiltered')
-%fontname('Times New Roman')
+fontname('Times New Roman')
 grid on
 subplot(3,1,2)
 hold on
@@ -332,7 +310,7 @@ legend('\omega x','y_{obs}')
 xlabel('Time Series (s)')
 ylabel('Acceleration (m/s^{2})')
 title('Y-Axis Acceleration - Corrected and Unfiltered')
-%fontname('Times New Roman')
+fontname('Times New Roman')
 grid on
 subplot(3,1,3)
 hold on
@@ -342,12 +320,12 @@ legend('\omega x','y_{obs}')
 xlabel('Time Series (s)')
 ylabel('Acceleration (m/s^{2})')
 title('Y-Axis Acceleration - Corrected and Filtered')
-%fontname('Times New Roman')
+fontname('Times New Roman')
 grid on
 
 %% Trajectory Estimation
 
-heading = deg2rad(filtered_mag_yaw)' - deg2rad(20);
+heading = deg2rad(filtered_yaw) - deg2rad(160);
 delta_t = diff(tunnel1_times);
 forward_vel = [0; delta_t].*tunnel1_accel(:,1);
 forward_vel = 10*forward_vel;
@@ -357,7 +335,7 @@ v_northing = forward_vel.*sin(heading);
 imu_northing = cumtrapz(tunnel1_times, v_northing);
 imu_easting = cumtrapz(tunnel1_times, v_easting);
 
-figure(11)
+figure(9)
 hold on
 plot(tunnel1_utmeast,tunnel1_utmnorth,'-*','LineWidth',1)
 plot(imu_easting,imu_northing,'LineWidth',1)
@@ -365,7 +343,7 @@ xlabel('Easting (m)')
 ylabel('Northing (m)')
 legend('GPS Trajectory','IMU Trajectory','Location','northwest')
 title('IMU and GPS Easting anf Northing Trajectories')
-%fontname('Times New Roman')
+fontname('Times New Roman')
 grid on
 
 %% Extract second IMU
